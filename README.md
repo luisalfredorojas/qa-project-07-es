2815c0ddbfcbce972bb6f49bcc0607158ff58a4c
luisrojas <luisrojasmunoz@gmail.com>
2024-04-18 03:12:24 -0500

    Entrega 1

---
+++ README.md
@@ -0,0 +1,48 @@
+# Proyecto 7
+
+Este es mi proyecto 7 de mi bootcamp de TripleTen. Donde aprendo y pongo en practica python junto con el IDE Pycharm y pytest para poder automatizar pruebas con selenium webdriver
+
+## <p>Configuración <br>
+Trabajarás con Git y GitHub en este proyecto.<br>
+
+Sigue los pasos a continuación para configurar tu proyecto.<p>
+## Paso 1: conecta tu GitHub<br>
+El primer paso es enlazar tu cuenta de GitHub a TripleTen. Para ello, haz clic en el botón "Enlazar la cuenta de GitHub" en el widget en la parte superior de esta página. Esto te llevará a una nueva pestaña del navegador donde se te pedirá que confirmes que deseas enlazar tu cuenta de GitHub. Si aún no has iniciado sesión en GitHub, se te pedirá que introduzcas tu nombre de usuario y contraseña. Al confirmarlo, tu perfil de TripleTen se conectará a tu perfil de GitHub a través de la API segura de GitHub. Esto te permitirá enviar tus proyectos automáticamente con tan solo hacer clic en un botón, directamente dentro de la plataforma de TripleTen.
+<br>
+## Paso 2. Clona el repositorio en tu computadora<br>
+Una vez que hayas vinculado tu cuenta de TripleTen con GitHub, se creará un repositorio automáticamente. El nombre del repositorio será qa-project-06-es.<br>
+Asegurate de crier otra rama y trabajes localmente en tu computadora<br>
+
+Ve a GitHub y clona el nuevo repositorio en tu computadora local, siguiendo estos pasos:<br>
+
+Abre la línea de comandos en tu computadora.<br>
+Si aún no lo has hecho, crea un directorio para almacenar todos tus proyectos. Por ejemplo:<br>
+>cd ~               # asegúrate de estar en tu directorio de inicio<br>
+>mkdir projects     # crea una carpeta llamada projects<br>
+>cd projects        # cambia el directorio a la nueva carpeta de proyectos<br>
+
+Clona el repositorio con SSH.<br>
+>git clone git@github.com:username/qa-project-07-es.git<br>
+
+💡 Asegúrate de clonar el repositorio correcto. El nombre de usuario debe ser tu propio nombre de usuario, no tripleten-com.<br>
+
+## Paso 3. Trabaja con el proyecto de forma local<br>
+Ahora tienes una copia local del proyecto y puedes abrir la carpeta del proyecto en tu computadora.<br>
+
+💡 Puedes utilizar PyCharm para trabajar en el proyecto localmente. Simplemente abre PyCharm y selecciona Archivo → Abrir y luego selecciona la carpeta qa-project-06 que clonaste en tu computadora.
+
+## Paso 4: Libreria Pytest y webdriver de selenium
+Asegurate de instalar este par de librerias para que el proyecto compile de la forma correcta.
+Ingresa en Python packages y busca requests y purest para poder instalarlos
+
+## Paso 5: Asegurate que pycharm reconozca las funciona con "test"
+
+## Paso 6: Instala el paquete Selenium 14.19.0
+Instala la ultima version del paquete de selenium en al seccion Python Packages.
+
+## Paso 7: Instala los webdrivers de tu preferencia
+Necestias webdrives de diferentes navegadores para poder trabajar y que selenium simule las pruebas.
+Descargados mediante este enlace https://www.selenium.dev/documentation/webdriver/troubleshooting/errors/driver_location/
+
+
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import data
+import time
 from selenium import webdriver
-from selenium.webdriver import Keys
+from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
@@ -35,8 +36,47 @@ def retrieve_phone_code(driver) -> str:
 
 
 class UrbanRoutesPage:
+
     from_field = (By.ID, 'from')
     to_field = (By.ID, 'to')
+    ask_taxi = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[1]/div[3]/div[1]/button')
+    comfort_button = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[1]/div[5]/div[1]/img')
+    type_picker = (By.CSS_SELECTOR, '.type-picker shown')
+
+
+#Numero de telfono
+    number_open_modal = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[1]/div')
+    number_modal = (By.XPATH, '//*[@id="root"]/div/div[1]/div[2]/div[1]')
+    #Darle clikc a este para poder poner el numero de telefono
+    number_input_click = (By.XPATH, '//*[@id="root"]/div/div[1]/div[2]/div[1]/form/div[1]/div[1]/label')
+    number_input = (By.ID, 'phone')
+    number_input_label = (By.XPATH, '//*[@id="root"]/div/div[1]/div[2]/div[2]/form/div[1]/div[1]')
+    number_modal_next = (By.XPATH, '//*[@id="root"]/div/div[1]/div[2]/div[1]/form/div[2]/button')
+    number_modal_code_next = (By.XPATH, '//*[@id="root"]/div/div[1]/div[2]/div[2]/form/div[2]/button[1]')
+
+#Metodo de pago
+    payment_method = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[2]')
+    payment_method_modal = (By.XPATH, '//*[@id="root"]/div/div[2]/div[2]/div[1]')
+    add_card_modal = (By.CSS_SELECTOR, '.pp-plus-container')
+    add_card_modal_inputs = (By.XPATH, '//*[@id="root"]/div/div[2]/div[2]/div[2]')
+    add_card_number_input = (By.ID, 'number')
+    add_card_CVV = (By.ID, 'code')
+    add_card_CVV_XPATH = (By.XPATH, '//*[@id="root"]/div/div[2]/div[2]/div[2]/form/div[1]/div[2]/div[2]/div[2]')
+    add_card_button = (By.XPATH, '//*[@id="root"]/div/div[2]/div[2]/div[2]/form/div[3]/button[1]')
+    close_payment_method_modal = (By.XPATH, '//*[@id="root"]/div/div[2]/div[2]/div[1]/button')
+    payment_method_value = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[2]/div[2]/div[1]')
+    click_out_of_focus = (By.XPATH, '//*[@id="root"]/div/div[2]/div[2]/div[2]/div')
+
+#Mensaje para el conductor
+    message_driver_click = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[3]/div')
+    message_driver_input = (By.ID, 'comment')
+
+#Requisitos para el pedido
+    requirements = (By.CSS_SELECTOR, 'reqs-header')
+    requirements_ice_cream_mantle_options = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[2]/div')
+    requirements_switch = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[1]/div/div[2]/div')
+    requirements_plus_iceCream = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[3]/div/div[2]/div[1]/div/div[2]/div/div[3]')
+
 
     def __init__(self, driver):
         self.driver = driver
@@ -53,6 +93,104 @@ class UrbanRoutesPage:
     def get_to(self):
         return self.driver.find_element(*self.to_field).get_property('value')
 
+    def get_personal(self):
+        return self.driver.find_element(*self.to_field)
+
+#Pedir un taxi
+    def ask_taxi_click(self):
+        self.driver.find_element(*self.ask_taxi).click()
+
+    def type_picker_section(self):
+        WebDriverWait(self.driver, 3).until(expected_conditions.visibility_of_element_located(self.type_picker))
+
+
+#Pedir tarifa comfort
+    def click_comfort(self):
+        self.driver.find_element(*self.comfort_button).click()
+
+#Agregar numero de telefono
+    def open_modal_phone_number(self):
+        self.driver.find_element(*self.number_open_modal).click()
+
+    #esta funcion sirve para habilitar el input y poder escribir. (poner await para poder empezar a escrbiir)
+    def click_label_phone_number(self):
+        self.driver.find_element(*self.number_input_click).click()
+
+    def set_phone_number(self):
+        self.driver.find_element(*self.number_input).send_keys(data.phone_number)
+
+    def get_phone_number(self):
+        return self.driver.find_element(*self.number_open_modal).text
+
+    def click_next_phone_number(self):
+        self.driver.find_element(*self.number_modal_next).click()
+
+    def click_label_code(self):
+        self.driver.find_element(*self.number_input_label).click()
+
+    def set_code(self):
+        self.driver.find_element(*self.add_card_CVV).send_keys(retrieve_phone_code(self.driver))
+
+    def click_next_phone_number_code(self):
+        self.driver.find_element(*self.number_modal_code_next).click()
+
+#Payment Method
+
+    def click_open_modal_payment_method(self):
+        self.driver.find_element(*self.payment_method).click()
+
+    def click_label_card_number(self):
+        self.driver.find_element(*self.add_card_number_input).click()
+
+    def click_add_card_inside_modal(self):
+        self.driver.find_element(*self.add_card_modal).click()
+
+    def set_payment_method_card_number(self):
+        self.driver.find_element(*self.add_card_number_input).send_keys(data.card_number)
+        self.driver.find_element(*self.add_card_number_input).send_keys(Keys.TAB)
+
+    def click_label_cvv(self):
+        self.driver.find_element(*self.add_card_CVV_XPATH).click()
+
+    def set_cvv(self):
+        self.driver.find_element(*self.add_card_CVV).send_keys(data.card_code)
+
+    def click_next_payment_method(self):
+        self.driver.find_element(*self.add_card_button).click()
+
+    def close_payment_method_click(self):
+        self.driver.find_element(*self.close_payment_method_modal).click()
+
+    def get_payment_method_value(self):
+        return self.driver.find_element(*self.payment_method_value).text
+
+    def click_out_of_focus_header(self):
+        self.driver.find_element(*self.click_out_of_focus).click()
+
+
+ #Mensaje al conductor
+
+    def click_label_message_driver(self):
+        self.driver.find_element(*self.message_driver_click).click()
+
+    def set_message_to_driver(self):
+        self.driver.find_element(*self.message_driver_input).send_keys(data.message_for_driver)
+
+#requisitos de manta y helado
+
+    def set_requirements(self):
+        self.driver.find_element(*self.requirements).click()
+
+    def click_requirements_ice_cream_option(self):
+        self.driver.find_element(*self.requirements_ice_cream_mantle_options).click()
+
+    def set_requirements_mantle(self):
+        self.driver.find_element(*self.requirements_switch).click()
+
+    def set_requirements_ice_cream(self):
+        self.driver.find_element(*self.requirements_plus_iceCream).click()
+        self.driver.find_element(*self.requirements_plus_iceCream).click()
+
 
 
 class TestUrbanRoutes:
@@ -61,20 +199,93 @@ class TestUrbanRoutes:
 
     @classmethod
     def setup_class(cls):
+        import time
         # no lo modifiques, ya que necesitamos un registro adicional habilitado para recuperar el código de confirmación del teléfono
         from selenium.webdriver import DesiredCapabilities
         capabilities = DesiredCapabilities.CHROME
         capabilities["goog:loggingPrefs"] = {'performance': 'ALL'}
-        cls.driver = webdriver.Chrome(desired_capabilities=capabilities)
+        cls.driver = webdriver.Chrome()
+        time.sleep(5)
+
 
     def test_set_route(self):
         self.driver.get(data.urban_routes_url)
+        time.sleep(5)
         routes_page = UrbanRoutesPage(self.driver)
         address_from = data.address_from
         address_to = data.address_to
-        routes_page.set_route(address_from, address_to)
+        #routes_page.set_route(address_from, address_to)
+        routes_page.set_from(address_from)
+        routes_page.set_to(address_to)
         assert routes_page.get_from() == address_from
         assert routes_page.get_to() == address_to
+#prueba para agregar el tipo de tarifa
+    def test_set_comfort_type(self):
+        routes_page = UrbanRoutesPage(self.driver)
+        time.sleep(3)
+        routes_page.ask_taxi_click()
+        time.sleep(3)
+        routes_page.click_comfort()
+        assert routes_page.type_picker_section
+#Prueba para agregar numero de telefono
+    def test_set_phone_number(self):
+
+        routes_page = UrbanRoutesPage(self.driver)
+        time.sleep(3)
+        routes_page.open_modal_phone_number()
+        time.sleep(2)
+        routes_page.click_label_phone_number()
+        time.sleep(1)
+        routes_page.set_phone_number()
+        time.sleep(2)
+        routes_page.click_next_phone_number()
+        time.sleep(1)
+        routes_page.click_label_code()
+        time.sleep(1)
+        routes_page.set_code()
+        time.sleep(1)
+        routes_page.click_next_phone_number_code()
+        time.sleep(3)
+        assert routes_page.get_phone_number() == data.phone_number
+#Prueba para agregar metodo de pago
+    def test_set_payment_method(self):
+        routes_page = UrbanRoutesPage(self.driver)
+        time.sleep(2)
+        routes_page.click_open_modal_payment_method()
+        time.sleep(2)
+        routes_page.click_add_card_inside_modal()
+
+        #time.sleep(2)
+        #routes_page.click_label_cvv()
+        time.sleep(2)
+        routes_page.set_payment_method_card_number()
+        time.sleep(6)
+        routes_page.set_cvv()
+
+        time.sleep(6)
+
+        routes_page.click_next_payment_method()
+        time.sleep(2)
+        routes_page.close_payment_method_click()
+        time.sleep(2)
+        assert routes_page.get_payment_method_value() == 'Tarjeta'
+#Prueba para mensaje al conductor
+    def test_set_message_to_driver(self):
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.click_label_message_driver()
+        time.sleep(2)
+        routes_page.set_message_to_driver()
+        assert routes_page.message_driver_input == data.message_for_driver
+#Prueba para poner los requisitos
+    def test_set_mantle_and_ice_cream(self):
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.set_requirements()
+        time.sleep(2)
+        routes_page.click_requirements_ice_cream_option()
+        time.sleep(2)
+        routes_page.set_requirements_mantle()
+        time.sleep(2)
+        routes_page.set_requirements_ice_cream()
 
 
     @classmethod
